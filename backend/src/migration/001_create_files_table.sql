3d0c6b8d90963764fa62834e5c06d024924b3cbf
Aldinokhalifah <khalifahaldino@gmail.com>
2026-08-15 20:29:00 +0700

    Menambahkan tabel files untuk menyimpan informasi file yang diunggah pengguna

---
+++ backend/src/migration/001_create_files_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE files (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    path_file TEXT NOT NULL,
+    name_file TEXT NOT NULL,
+    type VARCHAR(200) NOT NULL,
+    ukuran_byte BIGINT NOT NULL CHECK (ukuran_byte >= 0),
+    uploaded_at TIMESTAMP DEFAULT NOW(),
+    status VARCHAR(20) NOT NULL CHECK (status IN('pending', 'completed', 'failed'))
+)
